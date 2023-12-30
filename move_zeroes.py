c22091d46947159b16e7285d5b6c1a7aa5426de1
Aarafat Islam <99079792+aarafat27@users.noreply.github.com>
2023-12-30 18:55:06 +0600

    Create move_zeroes.py

---
+++ move_zeroes.py
@@ -0,0 +1,11 @@
+'''
+problem link: https://leetcode.com/problems/move-zeroes/
+'''
+
+class Solution:
+    def moveZeroes(self, nums: List[int]) -> None:
+        slow = 0
+        for fast in range(len(nums)):
+            if nums[fast] != 0:
+                nums[slow], nums[fast] = nums[fast], nums[slow]
+                slow += 1
